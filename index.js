46bcfa9a5f15868d866d8bf91960d059aeb7b261
AtsushiM <at.mizoue@gmail.com>
2024-11-08 07:45:26 +0900

    feat: word-break: break-all, word-break: brake-word の利用を禁止する

--- index.js
+++ index.js
@@ -17,6 +17,19 @@ module.exports = {
     'selector-class-pattern': null,
     'value-no-vendor-prefix': true,
     'property-no-vendor-prefix': true,
-    'no-empty-source': null
+    'no-empty-source': null,
+    'declaration-property-value-disallowed-list': [
+      {
+        'word-break': ['break-all', 'break-word'],
+      },
+      {
+        'message': `"word-break: break-all;" "word-break: break-word" 以下の理由で非推奨です
+ - "break-all" は英単語などが不自然に改行される可能性があるため非推奨です。
+ - "break-word" は仕様として非推奨になっています
+ - いずれの場合も以下で対応可能です
+   - "overflow-wrap: anywhere;" を利用することでなるべく単語を改行せず、かつどうしても横幅に収まらない場合のみ単語内で改行されるようになります。
+   - "overflow-wrap: anywhere;" だけではうまく改行指定出来ない場合、"word-break: normal;" も同時に指定すると改善する場合があります。`
+      }
+    ]
   }
 }
